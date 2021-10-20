13842cbac21a547f9d3a5a847db641fbe67b853b
Gleb Meshkov <gdmehkov@mail.ru>
2021-10-20 13:57:04 +0300

    Changes by Code-desaster

--- defs.py
+++ defs.py
@@ -67,6 +67,70 @@ def understand():
         result['scene'] = scene
         result['img'] = img
         result['text'] = text
-
+        print('GGGG', result)
         con.commit()
         return (result)
+
+
+def new_understand(req):
+    result = dict()
+    con = sql.connect('game.db')
+    cur = con.cursor()
+    ips_ = cur.execute("""SELECT ip FROM users1""").fetchall()
+    ips = []
+    [ips.append(x[0]) for x in ips_]
+    if req in ips:
+        scene = cur.execute('''SELECT scene FROM users1 WHERE ip = ?''', (req,)).fetchone()[0]
+    else:
+        scene = 'begin'
+        cur.execute(
+            """INSERT INTO users1 (IP, name, inv, hp, DMG, scene, armor, COIN) VALUES(?, '', '', ?, ?, ?, 100, 100)""",
+            (req, 0, 0, 'begin',))
+        con.commit()
+    html = cur.execute('''SELECT html FROM scene1 WHERE name = ?''', (scene,)).fetchone()[0]
+    paths = cur.execute("""SELECT paths FROM scene1 WHERE name=?""", (scene,)).fetchall()[0]
+    # taking images
+
+    img = 'pictures/' + cur.execute("""SELECT img FROM scene1 WHERE name=?""", (scene,)).fetchone()[0]
+    text = cur.execute("""SELECT text FROM scene1 WHERE name=?""", (scene,)).fetchone()[0]
+    biom = cur.execute("""SELECT biom FROM scene1 WHERE name=?""", (scene,)).fetchone()[0]
+    img_link = "{{ url_for('static', filename='pictures/" + str(img[0]) + "') }}"
+    lst = paths[0].split(",")
+
+    a = []
+    [a.append(eval(x[0])) for x in cur.execute("""SELECT mobs FROM scene1 WHERE name=?""", (scene,)).fetchall()]
+    av_mobs = cur.execute('''SELECT mobs FROM bioms WHERE name = ? ''', (biom,)).fetchall()[0][0].split(';')
+    a = a[0]
+    new_a = a.copy()
+    drop_lst = {}
+    for key, value in new_a.items():
+        if key not in av_mobs:
+            del a[key]
+            mob_drop = cur.execute('''SELECT stuff FROM mobs WHERE name = ?''', (key,)).fetchall()
+            for i in range(len(mob_drop)):
+                final_lst = mob_drop[i][0].split(';')
+                for j in range(len(final_lst)):
+                    new_lst = final_lst[j].split('*')
+                    n = 1
+                    el = final_lst[j]
+                    if len(new_lst) != 1:
+                        n = int(new_lst[1])
+                        el = new_lst[0]
+                    try:
+                        drop_lst[el] += n * int(value)
+                    except KeyError:
+                        drop_lst[el] = 0
+                        drop_lst[el] += n * int(value)
+
+    result['html'] = html
+    result['paths'] = lst
+    result['scene'] = scene
+    result['img'] = img
+    result['text'] = text
+    result['biom'] = biom
+    result['mobs'] = a
+    result['drop'] = drop_lst
+    con.commit()
+    return (result)
+
+print("nowshit",new_understand('127.0.0.1'))
--- main.py
+++ main.py
@@ -3,11 +3,22 @@ from flask import Flask, render_template, request, redirect, url_for
 from defs import understand
 import os
 
+# добавить в бд картинки
 app = Flask(__name__, template_folder="templates")
 app.config["IMAGE_UPLOADS"] = "C:/Users/dvmes/PycharmProjects/game-master/static/pictures/"
 
-global name
+global name, amount
 name = "/"
+amount = 2
+global scene_name, scene_entity, scene_paths
+scene_entity = {}
+scene_name = ''
+# biom_list - это хуета где лежит список возможных биомов .. текст это описание .. сцена_биом это биом который выбрали
+global biom_list, scene_text, scene_biom
+scene_text = ''
+biom_list = ["Govno", "Zhopa"]
+scene_paths = ''
+
 
 @app.route(name)
 def start():
@@ -21,7 +32,7 @@ def start_post():
     con = sql.connect('game.db')
     cur = con.cursor()
     res = request.form.get('name')
-    av_scenes_ = cur.execute("""SELECT name FROM scene1""").fetchall()[0]
+    av_scenes_ = cur.execute("""SELECT name FROM scene1""").fetchall()
     av_scenes = []
     [av_scenes.append(x[0]) for x in av_scenes_]
     print(av_scenes, '////1224414', res)
@@ -37,38 +48,73 @@ def start_post():
 
 @app.route("/edit")
 def authour():
-    return render_template('edit.html')
+    entity_list = []
+    biom_list = []
+    con = sql.connect('game.db')
+    cur = con.cursor()
+    print(cur.execute('''SELECT mobs FROM bioms''').fetchall(), cur.execute('''SELECT mobs FROM bioms''').fetchone())
+    bebra = cur.execute('''SELECT name FROM bioms''').fetchall()
+    [biom_list.append(x[0]) for x in bebra]
+    a = []
+    [a.append(x[0].split(';')) for x in cur.execute('''SELECT mobs FROM bioms''').fetchall()]
+    print(a)
+    new = set()
+    for elem in a:
+        for elem_ in elem:
+            new.add(elem_)
+    entity_list = list(new)
+    print(entity_list)
+    return render_template('edit.html', entity_list=entity_list, scene_name=scene_name, entity=scene_entity,
+                           biom_list=biom_list, scene_text=scene_text, scene_paths=scene_paths)
 
 
 @app.route("/edit", methods=["POST"])
 def editor():
-    scene_name = "/" + request.form.get('scene_name')
-    scene_paths = request.form.get('scene_paths')
-    scene_text = request.form.get('scene_text')
-    scene_img = ''
-    if request.files:
-        scene_img = request.files['scene_img']
+    global scene_name, scene_paths, scene_entity
+    global amount, scene_text, scene_biom
+    print('YA YEBAL MAt` vkada', request.form.get('Send'))
+    if request.form.get('add') is not None:
+        amount += 1
+        scene_name = request.form.get('scene_name')
+        scene_entity[request.form.get('select_entity')] = request.form.get('select_amount')
+        scene_text = request.form.get('scene_text')
+        scene_paths = request.form.get('scene_paths')
+        print("ENTITY--", scene_entity)
+        return redirect('/edit')
+    elif request.form.get('Send') is not None:
 
-        scene_img.save(os.path.join(app.config["IMAGE_UPLOADS"], scene_img.filename))
+        scene_img = ''
+        scene_html = ""
+        scene_html = scene_name + ".html"
+        con = sql.connect('game.db')
+        cur = con.cursor()
+        print(scene_name, cur.execute('''SELECT name FROM scene''').fetchone(), '////')
+        scene_names = []
+        [scene_names.append(x[0]) for x in cur.execute('''SELECT name FROM scene1''').fetchall()]
+        print(scene_names)
+        # получаем всю хуйню
+        scene_name = request.form.get('scene_name')
+        scene_text = request.form.get('scene_text')
+        scene_paths = request.form.get('scene_paths')
+        scene_biom = request.form.get('select_biom')
+        # добавить проверку в андерстенд есть ли в биоме (по хуйне)
+        scene_entity[request.form.get('select_entity')] = request.form.get('select_amount')
+        print("Вся хуйня", scene_biom, scene_text, scene_name, scene_paths, scene_entity)
 
-
-    scene_html = ""
-    scene_html = scene_name + ".html"
-    print(scene_name, scene_paths, scene_img.filename, type(scene_img), 'DWADADAWDWADAWD')
-    con = sql.connect('game.db')
-    cur = con.cursor()
-    print(scene_name, cur.execute('''SELECT name FROM scene''').fetchone(), '////')
-    if scene_name not in cur.execute('''SELECT * FROM scene''').fetchall():
         try:
-            cur.execute("INSERT INTO scene VALUES (?, ?, ?, ?, ?)",
-                        (scene_name, scene_paths, scene_img.filename, scene_text, 'start.html'))
+            cur.execute("INSERT INTO scene1 VALUES (?, ?, ?, ?, ?, ?, ?)",
+                        (scene_name, scene_biom, str(scene_entity), scene_paths, 'start.html', scene_text,
+                         scene_img))
         except sql.IntegrityError:
-            cur.execute('''UPDATE scene
-                                   SET paths = ?, img = ?, lvl = ?, html = ?
+            print('HDWHDOUWAHOUWHDWADJLWADBNLWJDNWLJNDLAJNLAWNDDNLAWDNWLNADLJND')
+            cur.execute('''UPDATE scene1
+                                   SET paths = ?, img = ?, html = ?, biom = ?, mobs = ?, text = ?
                                    
-                                   WHERE name = ?''', (scene_paths, scene_img.filename, scene_text, 'copy_file.html', scene_name,))
-    con.commit()
-    return redirect('/')
+                                   WHERE name = ?''',
+                        (scene_paths, scene_img, 'start.html', scene_biom, str(scene_entity), scene_text, scene_name,))
+        con.commit()
+        return redirect('/')
+    # ^___^ nyaaa ya anime psih mne pohui ^ ____ ^
 
 
 if __name__ == "__main__":
